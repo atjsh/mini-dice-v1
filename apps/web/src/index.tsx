509c7c3164b6ae5ff6f786772b407c83857fccfb
ATJSH <letaem77@gmail.com>
2022-03-28 03:15:47 +0900

    fix: remove ie support

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -4,8 +4,6 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 import App from './App';
 import './styles/index.css';
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
 
 const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
 
@@ -30,7 +28,7 @@ window.addEventListener('resize', () => {
 setInterval(() => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-}, 500);
+}, 1000);
 
 ReactDOM.render(
   <React.StrictMode>
