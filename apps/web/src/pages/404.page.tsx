969479246b61ce17daba4f9db24c6796075fa5d7
ATJSH <letaem77@gmail.com>
2022-03-30 01:37:16 +0900

    fix: add style on 404 page

--- apps/web/src/pages/404.page.tsx
+++ apps/web/src/pages/404.page.tsx
@@ -1,8 +1,21 @@
+import { Link } from 'react-router-dom';
+import { ServiceLayout } from '../layouts/wide-service/service.layout';
+import { IndexPageURL } from './routes';
+
 export function FourOhFourPage() {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page not found</p>
-    </div>
+    <ServiceLayout>
+      <div className=" text-center">
+        <h1 className=" text-xl">404</h1>
+        <p className=" text-4xl">Page not found</p>
+        <p className="text-4xl">페이지를 찾을 수 없음</p>
+        <Link
+          className="text-2xl text-blue-500 hover:underline mt-10 block p-4"
+          to={IndexPageURL}
+        >
+          ← Mini Dice로 돌아가기
+        </Link>
+      </div>
+    </ServiceLayout>
   );
 }
