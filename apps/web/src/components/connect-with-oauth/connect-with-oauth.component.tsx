dc20f4ed5e7338abb6045ce2ea648e528374f8d9
ATJSH <letaem77@gmail.com>
2022-04-02 21:36:46 +0900

    feat: update wording

--- apps/web/src/components/connect-with-oauth/connect-with-oauth.component.tsx
+++ apps/web/src/components/connect-with-oauth/connect-with-oauth.component.tsx
@@ -18,10 +18,13 @@ export const ConnectWithOauthWidget: React.FC = () => {
     ) : (
       <div className="bg-white dark:md:bg-zinc-800 dark:bg-black rounded-3xl flex flex-col gap-3 py-7">
         <div className="flex flex-col gap-2 px-8">
-          <div className="font-bold">구글 계정과 연결하세요.</div>
+          <div className="font-bold">다시 로그인할 수 있도록 계정 유지하기</div>
           <div className="text-sm">
-            "바로 플레이"로 게임하고 있습니다. 로그아웃된 경우 다시 로그인할 수
-            없게 됩니다. 이 계정을 계속 유지시키려면 구글 계정과 이 계정을
+            "바로 플레이"로 게임하고 있습니다.{' '}
+            <span className=" font-bold">
+              로그아웃된 경우 다시 로그인할 수 없게 됩니다.
+            </span>{' '}
+            <br />이 계정을 계속 유지시키려면 구글 계정과 이 계정을
             연결시키세요.
           </div>
         </div>
