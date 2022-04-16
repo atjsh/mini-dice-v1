f943555035bf3c7e51288edf4107d06ec642945c
ATJSH <letaem77@gmail.com>
2022-04-16 12:20:56 +0900

    feat(web): 베타 버전 명시

--- apps/web/src/components/wordmark/wordmark.component.tsx
+++ apps/web/src/components/wordmark/wordmark.component.tsx
@@ -1,5 +1,12 @@
 export const WordmarkComponent: React.FC = () => (
   <>
-    Mini <span className="text-minidice_red">Dice</span>
+    Mini <span className="text-minidice_red">Dice</span>{' '}
+    <span
+      className={`text-xl tracking-normal font-normal ${
+        process.env.WEB_VERSION_KIND === 'beta' ? '' : 'hidden'
+      }`}
+    >
+      (베타)
+    </span>
   </>
 );
