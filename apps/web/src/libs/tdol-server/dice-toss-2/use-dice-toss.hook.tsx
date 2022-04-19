0ba1977f9767f844446a6328aa7a2ed48806ba7e
ATJSH <letaem77@gmail.com>
2022-04-20 01:19:58 +0900

    feat(web): update useDiceToss

--- apps/web/src/libs/tdol-server/dice-toss-2/use-dice-toss.hook.tsx
+++ apps/web/src/libs/tdol-server/dice-toss-2/use-dice-toss.hook.tsx
@@ -18,10 +18,6 @@ import {
   sleep,
 } from '../../../common/timing';
 
-function getRandomInteger(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 export const useDiceToss = () => {
   const [diceTossActivityStatus, setDiceTossActivityStatus] = useRecoilState(
     diceTossActivityStatusAtom,
