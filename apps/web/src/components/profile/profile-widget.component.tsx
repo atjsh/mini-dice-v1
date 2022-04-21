b2dfc900ea9f692a8b193d8ddd77ef1ec27baf68
ATJSH <letaem77@gmail.com>
2022-04-22 01:48:25 +0900

    feat(web): add show my email on profile

--- apps/web/src/components/profile/profile-widget.component.tsx
+++ apps/web/src/components/profile/profile-widget.component.tsx
@@ -26,7 +26,9 @@ export const ProfileWidget: React.FC = () => {
               {new Date(user.createdAt).toLocaleDateString('ko-kr')} 시작
             </div>
             <div
-              className="text-white cursor-pointer hover:underline w-fit"
+              className={`text-white w-fit ${
+                user.email ? 'cursor-pointer hover:underline' : ''
+              }`}
               onClick={() => setisEmailShowing(!isEmailShowing)}
             >
               {user.email ? (
