0e2ac2ff136a908a6192f278e737926c29b36d2c
ATJSH <letaem77@gmail.com>
2023-03-25 00:43:41 +0900

    feat: add server deploy script

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get('')
   root(): string {
-    return 'lifegame2021team@gmail.com';
+    return 'contact: lifegame2021team@gmail.com';
   }
 }
---
+++ builders/server/src/build-option.ts
@@ -0,0 +1,49 @@
+import { esbuildDecorators } from '@anatine/esbuild-decorators';
+import * as esbuild from 'esbuild';
+import path from 'path';
+import { envCopy } from './env-copy.js';
+import { esbuildProgressPulgin } from './esbuild-progress.js';
+
+const projectRoot = path.resolve(process.cwd(), '..', '..');
+
+export const buildOptions = {
+  entryPoints: [
+    {
+      in: `${projectRoot}/apps/server/src/main.ts`,
+      out: 'index',
+    },
+  ],
+  outdir: `${projectRoot}/apps/server/dist`,
+  tsconfig: `${projectRoot}/apps/server/tsconfig.json`,
+  format: 'esm',
+  platform: 'node',
+  target: 'node19',
+  outExtension: { '.js': '.mjs' },
+  bundle: true,
+  minify: true,
+  keepNames: true,
+  legalComments: 'none',
+  external: [
+    '@nestjs/microservices',
+    '@nestjs/platform-express',
+    '@nestjs/websockets',
+    '@fastify/static',
+    '@fastify/view',
+    '@mikro-orm/core',
+    '@nestjs/mongoose',
+    '@nestjs/sequelize',
+  ],
+  banner: {
+    /** https://github.com/evanw/esbuild/issues/1921#issuecomment-1152991694 */
+    js:
+      "import{createRequire}from'module';const require=createRequire(import.meta.url);" +
+      "import{fileURLToPath}from'node:url';import{dirname as __pathDirname}from'node:path';const __filename=fileURLToPath(import.meta.url);const __dirname=__pathDirname(__filename);",
+  },
+  plugins: [
+    esbuildDecorators({
+      tsconfig: `${projectRoot}/apps/server/tsconfig.json`,
+    }),
+    esbuildProgressPulgin(),
+    envCopy,
+  ],
+} satisfies esbuild.BuildOptions;
