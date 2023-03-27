91157cee0ac487efe1555ade5ba87f913de6d6d0
전성훈 / SUNG HOON CHON <letaem77@gmail.com>
2023-03-27 17:59:30 +0900

    feat: add beta badge

--- apps/web/src/components/wordmark/wordmark.component.tsx
+++ apps/web/src/components/wordmark/wordmark.component.tsx
@@ -1,20 +1,21 @@
 export const WordmarkComponent: React.FC<{ colored?: boolean }> = ({
   colored,
 }) => (
-  <>
-    <span className="whitespace-nowrap">Mini</span>{' '}
-    <span
+  <div className="flex gap-2 items-end justify-center w-full">
+    <div className="whitespace-nowrap">Mini</div>{' '}
+    <div
       className={`${
         colored == undefined || colored == true ? 'text-minidice_red' : ''
       } whitespace-nowrap`}
     >
-      {' '}
       Dice
-    </span>{' '}
+    </div>
     {import.meta.env.VITE_WEB_VERSION_KIND !== 'prod' && (
-      <span className=" font-light">Beta</span>
+      <div className="font-medium bg-gray-400 text-xs md:text-lg px-3 py-1 dark:text-black rounded-xl text-white h-fit">
+        Beta
+      </div>
     )}
-  </>
+  </div>
 );
 
 export const KoreanWordmarkComponent: React.FC<{ colored?: boolean }> = ({
